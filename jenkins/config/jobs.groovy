790848dc4938a22881c22f6884d498a672539c7e
Nha <hugodichoi@gmail.com>
2016-03-26 18:01:38 +0800

    jenkins: Add loop for jobs

--- jenkins/config/jobs.groovy
+++ jenkins/config/jobs.groovy
@@ -1,32 +1,46 @@
-folder('project-a') {
-    displayName('Project A')
-    description('Folder for project A')
-}
+/**
+ * JOB CONFIGURATIONS
+ */
+def jobs = [
+        [
+                id: "0815",
+                name: "projectname",
+                repository: "https://github.com/nhabuiduc/maven-samples.git",
+        ]
+].each { i ->
+    job_name = i['id'] + "-" + i['name'].replaceAll(" ", "-")
 
-
-job('project-a/build2') {
-    description('Builds the component')
-
-    scm {
-        git('https://github.com/nhabuiduc/maven-samples.git')
+    folder(job_name) {
+        displayName('Project ' + job_name)
+        description('Folder for ' + job_name)
     }
-    triggers {
-        scm('*/15 * * * *')
-    }
-    steps {
-        shell('''
-CONFIG_NAME=`echo $JOB_NAME | tr '[:upper:]' '[:lower:]'`
-export CONFIG_NAME=${CONFIG_NAME##*/}
-echo $CONFIG_NAME
-export STAGE=${CONFIG_NAME##*_}
-echo $STAGE
 
-# We can have sanity checks
-# e.g. make sure that we compile only ONCE in the build step
-# and not during deployments
-grep "mvn compile" bin/build*
 
-bash -e bin/${CONFIG_NAME}.sh
-''')
+    job( job_name + '/build') {
+        description('Builds the component')
+
+        scm {
+            git(i['repository'])
+        }
+        triggers {
+            scm('*/15 * * * *')
+        }
+        steps {
+            shell("""\
+                CONFIG_NAME=`echo \$JOB_NAME | tr '[:upper:]' '[:lower:]'`
+                export CONFIG_NAME=\${CONFIG_NAME##*/}
+                echo \$CONFIG_NAME
+                export STAGE=\${CONFIG_NAME##*_}
+                echo \$STAGE
+
+                # We can have sanity checks
+                # e.g. make sure that we compile only ONCE in the build step
+                # and not during deployments
+                grep "mvn compile" bin/build*
+
+                bash -e bin/\${CONFIG_NAME}.sh
+                """.stripIndent().trim()
+            )
+        }
     }
 }
